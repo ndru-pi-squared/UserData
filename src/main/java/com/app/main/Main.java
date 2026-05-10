4c3e33a77f9905302aee70dac677bb7029af9f83
ndru-pi-squared <adpitt93@gmail.com>
2026-05-09 23:46:14 -0400

    comment out testing code from Main

--- src/main/java/com/app/main/Main.java
+++ src/main/java/com/app/main/Main.java
@@ -1,13 +1,22 @@
 package com.app.main;
 
 import com.app.controller.AppController;
+//import com.app.logging.LogEvent;
+//import com.app.logging.LogLevel;
+//import com.app.logging.LoggerConfig;
+//import com.app.logging.LoggerService;
 
 //entry point main class to run the application
 public class Main {
 
     public static void main (String[] args){
-        System.out.println("Hello, world2!");
+        System.out.println("Hello, world!");
         AppController appController = new AppController(10);
         appController.run();
+        //LoggerService loggerService = new LoggerService();
+        //LogEvent event = new LogEvent(LocalDateTime.now(), LogLevel.INFO, "Application started");
+        //loggerService.logEvent(event);
+        //LoggerConfig loggerConfig = new LoggerConfig();
+        //loggerConfig.run();
     }
 }
