41851a4819978bd7e96889816622671d82e7f9ae
ndru-pi-squared <adpitt93@gmail.com>
2026-05-09 23:48:18 -0400

    add getters to LogEvent

--- src/main/java/com/app/logging/LogEvent.java
+++ src/main/java/com/app/logging/LogEvent.java
@@ -14,6 +14,17 @@ public class LogEvent {
         this.timestamp = timestamp;
     }
 
+    public LogLevel getLevel() {
+        return level;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 
     @Override
     public String toString() {
