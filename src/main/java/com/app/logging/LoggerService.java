a46c47f742595ef131ff347333f9f16d9037e008
ndru-pi-squared <adpitt93@gmail.com>
2026-05-11 14:03:07 -0400

    refactor logging so LoggerService manages LogToFile

--- src/main/java/com/app/logging/LoggerService.java
+++ src/main/java/com/app/logging/LoggerService.java
@@ -3,19 +3,22 @@ package com.app.logging;
 //service class to handle logging operations
 public class LoggerService {
 
+    LogToFile logToFile;
+
     public LoggerService(){
         //for now, just print a message to console when logger service is initialized
         System.out.println("Logger service initialized");
+        logToFile = new LogToFile();
     }
 
     public void logEvent(LogEvent event){
         //for now, just print the event to console with its level
         System.out.println(event.toString());
+        logToFile.append(event.toString());
     }
 
     public void run(){
         System.out.println("Running logger service");
-        //logger.info("yo whats up");
     }
 }
 
