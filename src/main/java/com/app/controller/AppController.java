201c14ea4ee20aa2af4969cd22be14e2aad0c276
ndru-pi-squared <adpitt93@gmail.com>
2026-05-11 14:17:51 -0400

    add planned logging events and account data io in appcontroller

--- src/main/java/com/app/controller/AppController.java
+++ src/main/java/com/app/controller/AppController.java
@@ -24,5 +24,14 @@ public class AppController {
             loggerService.logEvent(new LogEvent(LocalDateTime.now(), LogLevel.INFO, "Session initiated"));
             break; //to prevent infinite loop for now, will add actual flow control later
         }
+        //logging events
+        //loggerService.logEvent(new LogEvent(LocalDateTime.now(), LogLevel.INFO, "Session terminated by user"));
+        //loggerService.logEvent(new LogEvent(LocalDateTime.now(), LogLevel.INFO, "Session terminated by system"));
+        //loggerService.logEvent(new LogEvent(LocalDateTime.now(), LogLevel.INFO, "Account " + userName + " created"));
+        //loggerService.logEvent(new LogEvent(LocalDateTime.now(), LogLevel.INFO, "User entered input " + input + " into userName field in account creation form"));
+        //loggerService.logEvent(new LogEvent(LocalDateTime.now(), LogLevel.INFO, "User entered input " + input + " into userMood field in account creation form"));
+
+        //account io
+        //writeAccountToFile(Path.of("data/account.txt"), user.toFileFormat(), false);
     }
 }
