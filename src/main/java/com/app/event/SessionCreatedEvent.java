137e7a7a9c5d90a867b8535ee057ee2ce0778020
ndru-pi-squared <adpitt93@gmail.com>
2026-05-14 02:27:45 -0400

    add sessiontype to sessioncreatedevent, encapsulate and finalize fields, add getters

--- src/main/java/com/app/event/SessionCreatedEvent.java
+++ src/main/java/com/app/event/SessionCreatedEvent.java
@@ -2,15 +2,35 @@ package com.app.event;
 
 import java.time.LocalDateTime;
 
+import com.app.session.SessionType;
+
 //event class to represent a session created event, will be used to log session creation events and trigger any necessary actions in response to session creation
 public class SessionCreatedEvent implements Event {
-    public LocalDateTime sessionTimeInit;
-    public String userID;
-    public String role;
+    private final LocalDateTime sessionTimeInit;
+    private final String userID;
+    private final String role;
+    private final SessionType sessionType;
 
-    public SessionCreatedEvent(LocalDateTime sessionTimeInit, String userID, String role){
+    public SessionCreatedEvent(LocalDateTime sessionTimeInit, String userID, String role, SessionType sessionType){
         this.sessionTimeInit = sessionTimeInit;
         this.userID = userID;
         this.role = role;
+        this.sessionType = sessionType;
+    }
+
+    public LocalDateTime getSessionTimeInit() {
+        return sessionTimeInit;
+    }
+    
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public SessionType getSessionType() {
+        return sessionType;
     }
 }
