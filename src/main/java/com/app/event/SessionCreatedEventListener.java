67fe2ea2c37fb4c0f55d2678db093eae9f14e840
ndru-pi-squared <adpitt93@gmail.com>
2026-05-13 20:10:02 -0400

    add logging to SessionCreatedEventListener to display when sessions are created

--- src/main/java/com/app/event/SessionCreatedEventListener.java
+++ src/main/java/com/app/event/SessionCreatedEventListener.java
@@ -1,9 +1,22 @@
 package com.app.event;
 
+import java.time.LocalDateTime;
+
+import com.app.logging.LogEvent;
+import com.app.logging.LogLevel;
+import com.app.logging.LoggerService;
+
 //event listener class to handle session created events, will be registered with the event dispatcher to receive notifications when a session created event occurs and perform necessary actions in response to the event
 public class SessionCreatedEventListener implements EventListener<SessionCreatedEvent> {
+    LoggerService loggerService;
+
+    public SessionCreatedEventListener(LoggerService loggerService){
+        this.loggerService = loggerService;
+    }
+    
     @Override
     public void onEvent(SessionCreatedEvent event) {
         // Handle session created event
+        loggerService.logEvent(new LogEvent(LocalDateTime.now(), LogLevel.INFO, "Session initiated"));
     }
 }
