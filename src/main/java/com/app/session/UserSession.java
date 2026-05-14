f016793783e6f64030a27ad46925fde587802dab
ndru-pi-squared <adpitt93@gmail.com>
2026-05-13 20:07:30 -0400

    usersession implements session interface. add overrides. add field sessionstate. encapsulate fields sessiontimeinit, inactivetimeinit, userid, role, and sessionstate. make sessiontimeinit, userid, and role immutable.

--- src/main/java/com/app/session/UserSession.java
+++ src/main/java/com/app/session/UserSession.java
@@ -3,27 +3,44 @@ package com.app.session;
 import java.time.LocalDateTime;
 
 //user session class to represent a user session in the system, will be used to track user activity and manage session state
-public class UserSession { 
+public class UserSession implements Session { 
 
-    public LocalDateTime sessionTimeInit;
-    public LocalDateTime inactiveTimeInit;//placeholder to track when user becomes inactive, will be used to determine when to terminate session after inactivity threshold is reached
-    public boolean active;
+    final private LocalDateTime sessionTimeInit;
+    @SuppressWarnings({"FieldMayBeFinal", "unused"})
+    private LocalDateTime inactiveTimeInit;//placeholder to track when user becomes inactive, will be used to determine when to terminate session after inactivity threshold is reached
 
     @SuppressWarnings("unused")
-    String userID; 
+    final private String userID; 
     @SuppressWarnings("unused")
-    String role; 
+    final private String role; 
+    private SessionState sessionState; //placeholder to track session state, will be used to manage session lifecycle and determine when to log session termination events
     //boolean isauthenticated
     
     public UserSession(LocalDateTime sessionTimeInit, String userID, String role){
-        //for now, just print a message to console when session is initiated
         this.sessionTimeInit = sessionTimeInit;
         this.userID = userID;
         this.role = role;
-        this.active = true;
-        System.out.println("User session initiated");
+        sessionState = SessionState.ACTIVE;
     }
-    
+
+    @Override
+    public LocalDateTime getStartTime() {
+        return sessionTimeInit;
+    }
+
+    @Override
+    public boolean isActive() {
+        return sessionState == SessionState.ACTIVE;
+    }
+
+    public void activate() {
+        sessionState = SessionState.ACTIVE;
+    }
+
+    public void deactivate() {
+        sessionState = SessionState.INACTIVE;
+    }
+
     public void run(){
         //for now, just print a message to console when session is initiated
         System.out.println("User session initiated");
