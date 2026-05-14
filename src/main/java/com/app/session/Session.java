403fe65a8a2de25100d4d4542209fda088f8d4dd
ndru-pi-squared <adpitt93@gmail.com>
2026-05-13 20:01:56 -0400

    add session interface

---
+++ src/main/java/com/app/session/Session.java
@@ -0,0 +1,10 @@
+package com.app.session;
+
+//session interface to represent a generic session in the system, will be implemented by specific session classes to represent different types of sessions that can occur in the system
+
+import java.time.LocalDateTime;
+
+public interface Session {
+    LocalDateTime getStartTime();
+    boolean isActive();
+}
