458195605c9cc4bcf69fd3bbab777daa40df56e6
ndru-pi-squared <adpitt93@gmail.com>
2026-05-14 22:11:47 -0400

    add eventsnapshotter to appcontroller

--- src/main/java/com/app/controller/AppController.java
+++ src/main/java/com/app/controller/AppController.java
@@ -5,11 +5,12 @@ package com.app.controller;
 import java.time.LocalDateTime;
 
 import com.app.event.EventDispatcher;
+import com.app.event.EventSnapshotter;
 import com.app.event.SessionCreatedEvent;
 import com.app.event.SessionCreatedEventListener;
 import com.app.logging.LoggerService;
-import com.app.session.Session;
 import com.app.session.SessionFactory;
+import com.app.session.UserSession;
 //import com.app.event.SessionCreatedEvent;
 //import com.app.logging.LogEvent;
 //import com.app.logging.LogLevel;
@@ -20,19 +21,21 @@ public class AppController {
     private final LoggerService loggerService;
     private final SessionCreatedEventListener sessionCreatedEventListener;
     private final EventDispatcher eventDispatcher;
+    private final EventSnapshotter eventSnapshotter;
 
     public AppController(){
         loggerService = new LoggerService();
         sessionCreatedEventListener = new SessionCreatedEventListener(loggerService);
         eventDispatcher = new EventDispatcher(sessionCreatedEventListener);
+        eventSnapshotter = new EventSnapshotter();
     }
 
     public void run(){
         running = true;
         //TODO: wrap the following two lines in an event dispatcher
-        Session userSession = SessionFactory.createUserSession(LocalDateTime.now(), "userID", "role");
-        SessionCreatedEvent sessionCreatedEvent = userSession.toSessionCreatedEvent();//take a snapshot of the session created
-        eventDispatcher.dispatch(sessionCreatedEvent);//dispatch the session created event to any registered listeners
+        UserSession userSession = SessionFactory.createUserSession(LocalDateTime.now(), "userID", "role");
+        SessionCreatedEvent sessionCreatedEvent = eventSnapshotter.takeSnapshot(userSession);
+        eventDispatcher.dispatch(sessionCreatedEvent);
         while(running){
             break; //to prevent infinite loop for now, will add actual flow control later
         }
