00adef82530c654fe3e975c61e26667b97d94149
ndru-pi-squared <adpitt93@gmail.com>
2026-05-16 02:48:50 -0400

    refactor SessionCreatedEvent to reflect UserRole instead of SessionType

--- src/main/java/com/app/event/SessionCreatedEvent.java
+++ src/main/java/com/app/event/SessionCreatedEvent.java
@@ -2,20 +2,18 @@ package com.app.event;
 
 import java.time.LocalDateTime;
 
-import com.app.session.SessionType;
+import com.app.user.UserRole;
 
 //event class to represent a session created event, will be used to log session creation events and trigger any necessary actions in response to session creation
 public class SessionCreatedEvent implements Event {
     private final LocalDateTime sessionTimeInit;
     private final String userID;
-    private final String role;
-    private final SessionType sessionType;
+    private final UserRole role;
 
-    public SessionCreatedEvent(LocalDateTime sessionTimeInit, String userID, String role, SessionType sessionType){
+    public SessionCreatedEvent(LocalDateTime sessionTimeInit, String userID, UserRole role){
         this.sessionTimeInit = sessionTimeInit;
         this.userID = userID;
         this.role = role;
-        this.sessionType = sessionType;
     }
 
     public LocalDateTime getSessionTimeInit() {
@@ -26,11 +24,8 @@ public class SessionCreatedEvent implements Event {
         return userID;
     }
 
-    public String getRole() {
+    public UserRole getRole() {
         return role;
     }
 
-    public SessionType getSessionType() {
-        return sessionType;
-    }
 }
