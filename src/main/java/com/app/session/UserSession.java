5a88fd93b237b3ac00c428a9c05238d9a6161910
ndru-pi-squared <adpitt93@gmail.com>
2026-05-16 03:20:46 -0400

    refactor usersession, update to reflect userrole instead of sessiontype, remove string variable for role, rename getsessiontype to getrole and remove override, removed unused warning suppressions

--- src/main/java/com/app/session/UserSession.java
+++ src/main/java/com/app/session/UserSession.java
@@ -2,6 +2,8 @@ package com.app.session;
 
 import java.time.LocalDateTime;
 
+import com.app.user.UserRole;
+
 
 //user session class to represent a user session in the system, will be used to track user activity and manage session state
 public class UserSession implements Session { 
@@ -10,25 +12,23 @@ public class UserSession implements Session {
     @SuppressWarnings({"FieldMayBeFinal", "unused"})
     private LocalDateTime inactiveTimeInit;//placeholder to track when user becomes inactive, will be used to determine when to terminate session after inactivity threshold is reached
 
-    @SuppressWarnings("unused")
     final private String userID; 
-    @SuppressWarnings("unused")
-    final private String role; 
+    final private UserRole role;
     private SessionState sessionState; //placeholder to track session state, will be used to manage session lifecycle and determine when to log session termination events
-    final private SessionType sessionType;
     
-    public UserSession(LocalDateTime sessionTimeInit, String userID, String role){
+    
+    public UserSession(LocalDateTime sessionTimeInit, String userID, UserRole role){
         this.sessionTimeInit = sessionTimeInit;
         this.userID = userID;
         this.role = role;
         sessionState = SessionState.ACTIVE;
-        sessionType = SessionType.USER;
     }
 
     /*@Override
     public SessionCreatedEvent toSessionCreatedEvent() {//handled in event snapshotter class to decouple
         return new SessionCreatedEvent(sessionTimeInit, userID, role, sessionType);
     }*/
+
     @Override
     public LocalDateTime getStartTime() {
         return sessionTimeInit;
@@ -39,17 +39,12 @@ public class UserSession implements Session {
         return sessionState == SessionState.ACTIVE;
     }
 
-    @Override
-    public SessionType getSessionType() {
-        return sessionType;
+    public UserRole getRole() {
+        return role;
     }
 
     public String getUserID() {
         return userID;
-    }
-
-    public String getRole() {
-        return role;
     }
 
     public void activate() {
