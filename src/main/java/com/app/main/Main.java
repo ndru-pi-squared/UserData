bd1766df95d2ba39efda6e7940678b3ef5df9590
ndru-pi-squared <adpitt93@gmail.com>
2026-05-16 03:27:32 -0400

    refactor main, add userrole as paramter of appcontroller

--- src/main/java/com/app/main/Main.java
+++ src/main/java/com/app/main/Main.java
@@ -1,6 +1,7 @@
 package com.app.main;
 
 import com.app.controller.AppController;
+import com.app.user.UserRole;
 //import com.app.logging.LogEvent;
 //import com.app.logging.LogLevel;
 //import com.app.logging.LoggerConfig;
@@ -11,7 +12,7 @@ public class Main {
 
     public static void main (String[] args){
         System.out.println("Hello, world!");
-        AppController appController = new AppController();
+        AppController appController = new AppController(UserRole.USER); //for now we just pass in a user role of USER, but in the future we can add authentication logic to determine the user role based on user input or other factors
         appController.run();
     }
 }
