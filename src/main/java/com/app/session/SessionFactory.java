13ea83d066c18d7ff770c03068e3298a5807eba4
ndru-pi-squared <adpitt93@gmail.com>
2026-05-17 02:02:54 -0400

    refactory SessionFactory change sessionInitTime to sessionStartTime to reflect common naming conventions

--- src/main/java/com/app/session/SessionFactory.java
+++ src/main/java/com/app/session/SessionFactory.java
@@ -7,8 +7,8 @@ import com.app.user.UserRole;
 //session factory class to create different types of sessions in the system, will be used to centralize session creation logic and provide a single point of entry for creating sessions in the system
 public class SessionFactory {
 
-    public static UserSession createUserSession(LocalDateTime sessionInitTime, String userId, UserRole role) {
-        return new UserSession(sessionInitTime, userId, role);
+    public static UserSession createUserSession(LocalDateTime sessionStartTime, String userId, UserRole role) {
+        return new UserSession(sessionStartTime, userId, role);
     }
 
     //public static Session createDatabaseSession(String connectionString) { //just for example, for now we only have one type of session but i just wanted to use a factory pattern here to demonstrate how we can use it to create different types of sessions in the future if needed
